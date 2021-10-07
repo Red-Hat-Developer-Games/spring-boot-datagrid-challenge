75b77ec5b1484087cb8ace6913a0c236419c040a
user2 <fabrizio.dutra@gruposantander.com>
2021-10-07 18:07:14 +0000

    user2

--- src/main/java/com/redhat/developergames/WeatherApp.java
+++ src/main/java/com/redhat/developergames/WeatherApp.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.dekorate.openshift.annotation.OpenshiftApplication;
 
+@OpenshiftApplication
 @SpringBootApplication
 @RestController
 @EnableCaching
